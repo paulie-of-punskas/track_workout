77f630c349c39e7e8430008de886446fa58e3148
paulie-of-punskas <pbernecki@protonmail.com>
2024-05-20 12:06:40 +0200

    hardcoduoti pratimai

---
+++ track_workout/static/exercises.py
@@ -0,0 +1,5 @@
+exercises = dict(krutine=("spaudimas", "drugeliai"),
+                 peciai=("sonai", "spaudimas"),
+                 tricepsas=("virve"),
+                 nugara=("prisitraukimai", "pulldown", "irklavimas"),
+                 bicepsas=("prisitraukimai", "hanteliai"))
